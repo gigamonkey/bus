7a37076114729c2c38a6433ab920df14f22ee1ed
Peter Seibel <peter@gigamonkeys.com>
2023-02-17 08:39:31 -0800

    Prettying

--- Makefile
+++ Makefile
@@ -5,8 +5,7 @@ files += script.js
 all: pretty lint
 
 pretty:
-	prettier -w --print-width 120 *.js
-	tidy -i -q -w 80 -m *.html
+	prettier -w --print-width 120 *.js *.html *.css
 
 lint:
 	npx eslint *.js
--- script.js
+++ script.js
@@ -1,38 +1,37 @@
 const token = '3F7F44FFEDE5D6A98FB4443BF380514F';
-const url = `https://api.actransit.org/transit/actrealtime/prediction?stpid=51583&rt=51B&token=${token}`
+const url = `https://api.actransit.org/transit/actrealtime/prediction?stpid=51583&rt=51B&token=${token}`;
 
 const body = document.body;
 const times = document.getElementById('times');
 
 const extractPredictions = async (resp) => {
   const data = await resp.json();
-  return data["bustime-response"].prd;
+  return data['bustime-response'].prd;
 };
 
 const time = (p) => {
-  const t = document.createElement("p");
+  const t = document.createElement('p');
   t.append(`${p.prdctdn} minutes`);
   return t;
-}
+};
 
 const fetchPredictions = async () => {
-  const predictions = await fetch(url).then(extractPredictions)
+  const predictions = await fetch(url).then(extractPredictions);
   times.replaceChildren(...predictions.map(time));
 };
 
 const totalHeight = (e) => [...e.children].reduce((acc, c) => acc + c.offsetHeight, 0);
 
-await fetchPredictions()
+await fetchPredictions();
 
 const target = body.clientHeight * 0.8;
 
 for (let s = 0; s < 200; s++) {
   body.style.fontSize = `${s}px`;
   if (totalHeight(body) > target) {
-    body.style.fontSize = `${s-1}px`;
+    body.style.fontSize = `${s - 1}px`;
     break;
   }
 }
-
 
 setInterval(fetchPredictions, 30000);
